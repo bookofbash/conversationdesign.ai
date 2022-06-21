5960d4fa862f2b1984b1a468540173817ce99ba6
Bashir Harrell <bashir@Bashirs-MacBook-Air.local>
2022-06-21 15:46:16 -0400

    Updated navigation for /resources

--- resources.js
+++ resources.js
@@ -2,12 +2,16 @@ console.log("Hello World")
 console.log("JavaScrpt is working")
 
 // Replace URL with your url
-const url = "./joblist.json";
-fetch(url)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// const url = "./joblist.json";
+// fetch(url)
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+  const resource_nav = document.getElementsByClassName('resource-nav')
+  const resource_nav_buttons = resource_nav.querySelector('.nav-item')
+  console.log(resource_nav_buttons)
