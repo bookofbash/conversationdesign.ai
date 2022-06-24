d6d831409ded70635a98d814f1a4bac090bb1f94
Bashir Harrell <bashir@Bashirs-MacBook-Air.local>
2022-06-24 15:57:12 -0400

    Updated background color in resources.css

--- resources.js
+++ resources.js
@@ -1,6 +1,28 @@
 console.log("Hello World")
 console.log("JavaScrpt is working")
 
+// function runOnScroll() {
+//   if (document.body.scrollTop =1000) {
+//     var h1 = document.getElementsByTagName("h1")[0];
+//     h1.style.fontSize = "10%";
+//     console.log ("You scrolling")
+//   }
+// };
+// window.addEventListener("scroll", runOnScroll);
+
+
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//     document.getElementById("header").style.fontSize = "30px";
+//   } else {
+//     document.getElementById("header").style.fontSize = "90px";
+//   }
+// }
+
+
 // Replace URL with your url
 // const url = "./joblist.json";
 // fetch(url)
@@ -11,7 +33,3 @@ console.log("JavaScrpt is working")
 //   .catch((error) => {
 //     console.log(error);
 //   });
-
-  const resource_nav = document.getElementsByClassName('resource-nav')
-  const resource_nav_buttons = resource_nav.querySelector('.nav-item')
-  console.log(resource_nav_buttons)
